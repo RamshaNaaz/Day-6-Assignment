6e4db5d7791361425b344e256da44480b199e218
RamshaNaaz <ramshanaaz93@gmail.com>
2022-05-12 20:24:27 +0530

    2 file in day 6

---
+++ pallin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+echo "Enter the number for checking"
+read num
+
+s=0
+rev=""
+
+temp=$num
+
+while [ $num -gt 0 ]
+do
+   s=$(( $num % 10 ))
+
+  num=$(( $num / 10 ))
+
+   rev=$( echo ${rev}${s} )
+done
+
+if [ $temp -eq $rev ]
+then
+echo "Number is a pallindrome"
+
+else
+echo "Not a pallindrome"
+
+fi
